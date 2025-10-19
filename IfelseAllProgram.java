f28ee8156a8f676e4405e83d4ffa8b32c6e01cbc
Yash Panchal <44561539+yashpanchal089@users.noreply.github.com>
2025-10-19 23:41:37 +0530

    All programof if-else

---
+++ IfelseAllProgram.java
@@ -0,0 +1,70 @@
+public class IfelseAllProgram {
+    public static void main(String[] args) {
+
+        // 1. Simple if statement
+        // int x = 10;
+        // int y = 20;
+        // if (x + y > y) {
+        //     System.out.println("x is greater than y");
+        // }   
+
+
+        // 2. If-else statement
+        // int a  = 20;
+        // int b = 100;
+        // if (a > b) {
+        //     System.out.println("a is greater than b");
+        
+        // } else {
+        //     System.out.println("b is greater than a");
+        // }
+
+        //3. if-else if-else ladder statement
+        // String city = "Delhi";
+        // if (city == "Meerut") {
+        //     System.out.println("city is meerut");
+        // } else if (city == "Noida") {
+        //     System.out.println("city is noida");
+        // } else if (city == "Agra") {
+        //     System.out.println("city is agra");
+        // } else {
+        //     System.out.println(city);
+        // }
+
+        // 4. Nested if -else statement
+        // int x = 25;
+        // int y = 30;
+        // int z = 35;
+        // if( x > y ) {
+        //     if ( y > z) {
+        //         System.out.println("Y ig Greater than Z");            }
+        //     }
+        //     else if (x > z) {
+        //         System.out.println("X is greater than Z");
+        //     }
+        //     else { 
+        //         System.out.println(" Y is greater than X");
+        //     } 
+
+
+        // if else if ladder example
+        // int score = 85;
+        // if (score >=80 && score <= 100){
+        //     System.out.println("Grade A");
+        // } else if (score >= 60 && score <= 80) {
+        //     System.out.println("Grade B");
+        // } else if (score >= 40 && score <= 60) {
+        //     System.out.println("Grade C");
+        // } else {
+        //     System.out.println("Try Hard For Exam");
+        // }
+
+        // Ternary Operator
+        // int number = 13;
+        // String output = (number%2==0)?"even number":"odd number";
+        // System.out.println(output);
+        
+    }
+}
+
+
