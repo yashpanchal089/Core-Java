da80a32002cf4f833d9cda4b54edd3d1352e2e78
Yash Panchal <44561539+yashpanchal089@users.noreply.github.com>
2025-10-19 23:43:28 +0530

    Enumeration

---
+++ Enumeration.java
@@ -0,0 +1,8 @@
+enum Day{Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday}
+
+public class Enumeration {
+    public static void main(String[] args) {
+        Day d = Day.Monday;
+        System.out.println(d);
+    }
+}
