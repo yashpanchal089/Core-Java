9ce49901119ee3d36960845c8d4803033cf12a14
Yash Panchal <44561539+yashpanchal089@users.noreply.github.com>
2025-10-22 23:16:33 +0530

    Add VotingEligibility class to check voting eligibility

---
+++ If-Else-Prpgram/VotingEligibility.java
@@ -0,0 +1,11 @@
+public class VotingEligibility {
+    public static void main(String[] args) {
+        int age = 17;
+
+        if (age >= 18) {
+            System.out.println("Eligible to vote");
+        } else {
+            System.out.println("Not eligible to vote");
+        }
+    }
+}
