15dca5f1d950b871a109a501f34aca4aeb73f70e
Yash Panchal <44561539+yashpanchal089@users.noreply.github.com>
2025-10-22 23:15:26 +0530

    Add NumberCheck class to evaluate integer sign

---
+++ If-Else-Prpgram/NumberCheck.java
@@ -0,0 +1,12 @@
+public class NumberCheck {
+    public static void main(String[] args) {
+        int num = -5;
+
+        if (num > 0) {
+            System.out.println("Positive number");
+        } else if (num < 0) {
+            System.out.println("Negative number");
+        } else {
+            System.out.println("Zero");
+        }
+    }
