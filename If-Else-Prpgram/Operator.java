a32d310387db415f4cf82dee24aa515bafaf46b5
Yash Panchal <44561539+yashpanchal089@users.noreply.github.com>
2025-10-22 23:19:35 +0530

    Add files via upload

---
+++ If-Else-Prpgram/Operator.java
@@ -0,0 +1,12 @@
+class Operator {
+    
+    
+    
+    
+    public static void main(String[] args) {
+        int a = 555;
+        int b = 10;
+        int c = a + b;
+        System.out.println(c);
+    }
+}
