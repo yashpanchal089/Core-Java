aa09edddca68039b161d9dd08a399d64bc533192
Yash Panchal <44561539+yashpanchal089@users.noreply.github.com>
2025-10-22 23:16:10 +0530

    Add LargestNumber class to compare two integers

---
+++ If-Else-Prpgram/LargestNumber.java
@@ -0,0 +1,11 @@
+public class LargestNumber {
+    public static void main(String[] args) {
+        int a = 25, b = 42;
+
+        if (a > b) {
+            System.out.println(a + " is larger");
+        } else {
+            System.out.println(b + " is larger");
+        }
+    }
+}
