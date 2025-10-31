b32f3b7892522e6d4e7ee364c9d4d61dfde43cd9
Yash Panchal <44561539+yashpanchal089@users.noreply.github.com>
2025-10-31 11:22:53 +0530

    Create Main.java

---
+++ Control Statement/For-Each Loop /Main.java
@@ -0,0 +1,10 @@
+class Main{  
+  public static void main(String args[]){  
+   //declaring an array  
+   int arr[]={12,13,14,44};  
+   //traversing the array with for-each loop  
+   for(int i:arr){  
+     System.out.println(i);  
+   }  
+ }   
+}  
