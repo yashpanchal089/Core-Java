d86f6a66fe1195f62162de55dd32c96cd25a8d23
Yash Panchal <44561539+yashpanchal089@users.noreply.github.com>
2025-12-02 23:51:40 +0530

    Add GradeCalculator to evaluate student grades

---
+++ If-Else-Prpgram/GradeCalculator.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Marks: ");
+        int marks = sc.nextInt();
+
+        if (marks >= 90) {
+            System.out.println("Grade: A+");
+        } else if (marks >= 80) {
+            System.out.println("Grade: A");
+        } else if (marks >= 70) {
+            System.out.println("Grade: B");
+        } else if (marks >= 60) {
+            System.out.println("Grade: C");
+        } else if (marks >= 50) {
+            System.out.println("Grade: D");
+        } else {
+            System.out.println("Grade: Fail");
+        }
+    }
+}
